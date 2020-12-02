fc46005b01e114e556ff1242120293352a00a26f
Draco18s <draco18s@gmail.com>
2020-12-01 19:39:27 -0600

    Day 1 Part 2

--- DayOne.cs
+++ DayOne.cs
@@ -17,5 +17,23 @@ namespace Advent_of_Code_2020 {
 			}
 			return -1;
 		}
+
+		internal static int Part2(string input) {
+			string[] p = input.Split(' ');
+			int[] ints = new int[p.Length];
+			for(int i = 0; i < p.Length; i++) {
+				ints[i] = int.Parse(p[i]);
+			}
+			for(int i = 0; i < p.Length; i++) {
+				for(int j = 0; j < p.Length; j++) {
+					for(int k = 0; k < p.Length; k++) {
+						if(ints[i] + ints[j] + ints[k] == 2020) {
+							return ints[i] * ints[j] * ints[k];
+						}
+					}
+				}
+			}
+			return -1;
+		}
 	}
 }
