a7736587589314d63181691b0b78ec76e4f6f9e6
Draco18s <draco18s@gmail.com>
2020-12-01 19:35:59 -0600

    Separate files for separate days

---
+++ DayOne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Advent_of_Code_2020 {
+	internal class DayOne {
+		internal static int Part1(string input) {
+			string[] p = input.Split(' ');
+			int[] ints = new int[p.Length];
+			for(int i = 0; i < p.Length; i++) {
+				ints[i] = int.Parse(p[i]);
+			}
+			for(int i = 0; i < p.Length; i++) {
+				for(int j = 0; j < p.Length; j++) {
+					if(ints[i] + ints[j] == 2020) {
+						return ints[i] * ints[j];
+					}
+				}
+			}
+			return -1;
+		}
+	}
+}
