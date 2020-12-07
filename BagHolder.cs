0bc8fb161b3d116baeeea7a011fa2f696001f2df
Draco18s <draco18s@gmail.com>
2020-12-06 23:33:10 -0600

    Day 7

---
+++ BagHolder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Advent_of_Code_2020 {
+	internal class BagHolder {
+		public readonly string name;
+		public Dictionary<string,int> canContain;
+		private static Regex containMatch = new Regex(@"(\d+) ([a-z ]+) bags*");
+
+		public BagHolder(string n, string contents) {
+			name = n;
+			canContain = new Dictionary<string,int>();
+			if(contents == "no other bags") {
+
+			}
+			else {
+				MatchCollection matches = containMatch.Matches(contents);
+				foreach(Match match in matches) {
+					GroupCollection groups = match.Groups;
+					string bagName = groups[2].Value;
+					int count;
+					int.TryParse(groups[1].Value, out count);
+					canContain.Add(bagName, count);
+				}
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 namespace Advent_of_Code_2020 {
 	class Program {
 		static void Main(string[] args) {
-			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day6.txt"));
+			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day7.txt"));
 			input = input.Replace("\r", "");
 			//input = "";
-			long result = DaySix.Part1(input);
+			long result = DaySeven.Part1(input);
 			Console.WriteLine(result);
-			result = DaySix.Part2(input);
+			result = DaySeven.Part2(input);
 			Console.WriteLine(result);
 			Console.ReadKey();
 		}
