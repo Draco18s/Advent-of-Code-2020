369dd108dc3ce545b813527891b7effcc33ead46
Draco18s <draco18s@gmail.com>
2020-12-07 23:28:01 -0600

    Day 8
    with comments

---
+++ ExecutionContext.cs
@@ -0,0 +1,34 @@
+﻿namespace Advent_of_Code_2020 {
+	internal class ExecutionContext {
+		private string[] lines;
+		private int pointer;
+		private int accumulator;
+
+		public ExecutionContext(string[] lines) {
+			this.lines = lines;
+			accumulator = 0;
+			pointer = 0;
+		}
+
+		public int Run(out int acc) {
+			string inst = lines[pointer]; // get instruction at pointer
+			string[] inst2 = inst.Split(' ');
+			if(inst2[0] == "nop") {
+				pointer++; // next
+			}
+			if(inst2[0] == "acc") {
+				int v;
+				int.TryParse(inst2[1], out v);
+				accumulator += v; // increment
+				pointer++; // next
+			}
+			if(inst2[0] == "jmp") {
+				int v;
+				int.TryParse(inst2[1], out v);
+				pointer += v; // jump to new instruction
+			}
+			acc = accumulator; // return accumulator
+			return pointer; // return pointer
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 namespace Advent_of_Code_2020 {
 	class Program {
 		static void Main(string[] args) {
-			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day7.txt"));
+			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day8.txt"));
 			input = input.Replace("\r", "");
 			//input = "";
-			long result = DaySeven.Part1(input);
+			long result = DayEight.Part1(input);
 			Console.WriteLine(result);
-			result = DaySeven.Part2(input);
+			result = DayEight.Part2(input);
 			Console.WriteLine(result);
 			Console.ReadKey();
 		}
