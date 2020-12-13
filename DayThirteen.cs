cef13ff484f4a42e95e016b96bcebda7299d7b99
Draco18s <draco18s@gmail.com>
2020-12-13 00:17:56 -0600

    Day 13
    Do half the work, then make wolfram alpha do the hard work.

---
+++ DayThirteen.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent_of_Code_2020 {
+	internal class DayThirteen {
+		internal static int Part1(string input) {
+			string[] lines = input.Split('\n');
+			int current = int.Parse(lines[0]);
+			lines = lines[1].Split(',');
+			int[] busIDs = new int[lines.Length];
+			for(int i=0; i<lines.Length;i++) {
+				string l = lines[i];
+				int v;
+				if(int.TryParse(l, out v)) {
+					busIDs[i] = v;
+				}
+			}
+			int minTime = int.MaxValue;
+			int minBus = -1;
+			foreach(int bus in busIDs) {
+				if(bus == 0) continue;
+				int nextLeave = (current / bus)+1;
+				if(nextLeave * bus < minTime) {
+					minTime = nextLeave * bus;
+					minBus = bus;
+				}
+			}
+			return minBus * (minTime - current);
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+			//int current = int.Parse(lines[0]);
+			lines = lines[1].Split(',');
+			int[] busIDs = new int[lines.Length];
+			for(int i = 0; i < lines.Length; i++) {
+				string l = lines[i];
+				int v;
+				if(int.TryParse(l, out v)) {
+					busIDs[i] = v;
+				}
+			}
+			int numPlus = 0;
+			Console.WriteLine("Plug this into wolframalpha:");
+			foreach(int x in busIDs) {
+				if(x == 0) {
+					numPlus++;
+					continue;
+				}
+				Console.Write("(x+" + numPlus + ")%" + x + "=0,");
+				numPlus++;
+			}
+			Console.Write("\n");
+			return -1;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ using System.Threading.Tasks;
 namespace Advent_of_Code_2020 {
 	class Program {
 		static void Main(string[] args) {
-			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day12.txt"));
+			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day13.txt"));
 			input = input.Replace("\r", "");
 			//input = @"";
-			long result = DayTwelve.Part1(input);
+			long result = DayThirteen.Part1(input);
 			Console.WriteLine(result);
 			DateTime s = DateTime.Now;
-			result = DayTwelve.Part2(input);
+			result = DayThirteen.Part2(input);
 			DateTime e = DateTime.Now;
 			Console.WriteLine(result);
 			Console.WriteLine((e-s).TotalMilliseconds);
