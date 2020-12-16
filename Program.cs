3c16edd8262b8c4dbc24d08e1a3edc3755e1a4a4
Draco18s <draco18s@gmail.com>
2020-12-16 00:22:22 -0600

    Day 16

--- Program.cs
+++ Program.cs
@@ -8,16 +8,16 @@ using System.Threading.Tasks;
 namespace Advent_of_Code_2020 {
 	class Program {
 		static void Main(string[] args) {
-			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day15.txt"));
+			string input = File.ReadAllText(Path.GetFullPath("../../inputs/Day16.txt"));
 			input = input.Replace("\r", "");
 			//input = @"";
 			DateTime s = DateTime.Now;
-			long result = DayFifteen.Part1(input);
+			long result = DaySixteen.Part1(input);
 			DateTime e = DateTime.Now;
 			Console.WriteLine(result);
 			Console.WriteLine("Time: " + (e - s).TotalMilliseconds);
 			s = DateTime.Now;
-			result = DayFifteen.Part2(input);
+			result = DaySixteen.Part2(input);
 			e = DateTime.Now;
 			Console.WriteLine(result);
 			Console.WriteLine("Time: " + (e-s).TotalMilliseconds);
