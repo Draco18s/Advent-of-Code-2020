ae06329fc7387786e3b52b9a348f115fa10cbe17
Draco18s <draco18s@gmail.com>
2020-12-15 20:41:25 -0600

    Update DayFifteen.cs
    Silly nonsense with goto. Can be ignored.

--- DayFifteen.cs
+++ DayFifteen.cs
@@ -48,8 +48,9 @@ namespace Advent_of_Code_2020 {
 			// input values are guaranteed unique, so the most recent number is always
 			// the first time it was said, so the first turn after input is always 0.
 			int next = 0;
+			int last = 0;
 			while(true) {
-				int last = next; // the "next" number becomes the "last" number
+				last = next; // the "next" number becomes the "last" number
 
 				// compute the new "next" value
 				if(dict.ContainsKey(last)) {
@@ -60,16 +61,46 @@ namespace Advent_of_Code_2020 {
 				}
 
 				// update the turn index for the "last" value
-				if(dict.ContainsKey(last)) {
-					dict[last] = turn;
-				}
-				else {
-					dict.Add(last, turn);
-				}
+				dict[last] = turn;
+
 				turn++; // incrememnt turn
+				
+				// check for the end of the game
 				if(turn == 30000000) { // 30 million
 					return last; // we need the "last" number spoken
 				}
+				
+				// Silly nonsense below here.
+				// Same code, but arranged in an order that reads well
+				// Then goto for actual program flow.
+				// Ignore the goto and the labels and just read the comments.
+				/*goto label4;
+
+			label3:
+				//increment turn counter
+				turn++;
+				goto label5;
+			label2:
+				//compute the next number based on the last
+				if(dict.ContainsKey(last)) {
+					next = turn - dict[last];
+				}
+				else {
+					next = 0;
+				}
+				goto label1;
+			label4:
+				last = next;
+				goto label2;
+			label1:
+				//store the number
+				dict[last] = turn;
+				goto label3;
+			label5:
+				// check for the end of the game
+				if(turn == 30000000) { // 30 million
+					return last; // we need the "last" number spoken
+				}*/
 			}
 		}
 	}
