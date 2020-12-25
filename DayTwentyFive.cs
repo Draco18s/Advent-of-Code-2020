34ea1d32d353fb1960f16bad11536d4822d9f806
Draco18s <draco18s@gmail.com>
2020-12-25 13:16:05 -0600

    Day 25

---
+++ DayTwentyFive.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Advent_of_Code_2020 {
+	internal class DayTwentyFive {
+		public static long Transform(long subjectNum, int loopSize) {
+			long value = 1;
+			for(; loopSize > 0; loopSize--) {
+				value *= subjectNum;
+				value = value % 20201227;
+			}
+			return value;
+		}
+
+		public static long Transform2(long value, long subjectNum) {
+			value *= subjectNum;
+			value = value % 20201227;
+			return value;
+		}
+
+		internal static long Part1(string input) {
+			string[] lines = input.Split('\n');
+			long cardInput = long.Parse(lines[0]);
+			long doorInput = long.Parse(lines[1]);
+
+			int testLoop = 102710;
+			int cardSecret = -1;
+			int doorSecret = -1;
+			long value = Transform(7, testLoop);
+			do {
+				testLoop++;
+				long cardPub = Transform2(value, 7);
+				value = cardPub;
+				if(cardPub == cardInput) {
+					cardSecret = testLoop;
+				}
+				if(cardPub == doorInput) {
+					doorSecret = testLoop;
+				}
+			} while(cardSecret < 0 || doorSecret < 0);
+			return Transform(cardInput, doorSecret);
+		}
+
+		internal static long Part2(string input) {
+			string[] lines = input.Split('\n');
+
+			return -1;
+		}
+	}
+}
